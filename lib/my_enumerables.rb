7f4441c17a8bb7ac5da0d914347e6b5be1dfb411
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-20 14:06:51 -0400

    Add my_each solution

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,11 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for element in self do
+      yield element
+    end
+  end
 end
