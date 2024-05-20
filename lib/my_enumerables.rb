ac0e59c35d69e761d603dfe926256f590ab4f5d7
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-20 17:45:39 -0400

    Add my_count solution and fix error in spec
    * Adds my solution for my_count
    
    * Fixes an error made prior that adds unneeded
    question marks to my_count

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,4 +1,16 @@
 module Enumerable
+  def my_count(arg = nil, &block)
+    count = 0
+    return self.size if !block_given? && arg.nil?
+
+    unless arg.nil?
+      self.my_each { |element| count += 1 if element == arg }
+      return count
+    end
+    self.my_each { |element| count += 1 if block.call(element) }
+    count
+  end
+
   def my_each
     return to_enum(:my_each) unless block_given?
 
