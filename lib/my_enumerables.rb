d5fe3a0e5d0124d891c93db8fb980acf71e50b7d
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-05-21 15:06:10 -0400

    Refactor array matching
    Refactors Enumerables for arrays

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -14,24 +14,18 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    for key, value in self do
-      yield key, value
+    for element in self do
+      yield element
     end
   end
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-    if is_a?(Array)
-      index = 0
-      for element in self do
-        yield element, index
-        index += 1
-      end
-    elsif is_a?(Hash)
-      for key, value in self do
-        yield key, value
-      end
+    index = 0
+    for element in self do
+      yield element, index
+      index += 1
     end
   end
 end
